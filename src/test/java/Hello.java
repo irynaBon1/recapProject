9d77e2027f9d5fadb21e97ccf11b4bd67c87cae9
IrynaBonchak <beatle600@gmail.com>
2021-06-30 18:31:56 -0500

    Working on recap

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("recaping like day 1");
+        System.out.println("recapping like day 1, I want to recap");
     }
 }
