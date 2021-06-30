3902ef8dcfe1c8ad4eb336bf0bdfd3d2d68deb82
IrynaBonchak <beatle600@gmail.com>
2021-06-30 18:46:02 -0500

    Started user story 100

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("recapping like day 1, I want to recap");
+        System.out.println("recapping like day 1, I want to recap more");
     }
 }
