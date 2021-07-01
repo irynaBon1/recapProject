7c2114d840c7e92df2d39e3a867b2960e6d03d15
IrynaBonchak <beatle600@gmail.com>
2021-06-30 20:24:26 -0500

    user story 102 more changes

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -2,5 +2,5 @@ public class UserStory102 {
     //cod was written here
     ///ready to push this branch remote
     // and open pull request so I can start collaboration
-
+//more changes added as requested
 }
